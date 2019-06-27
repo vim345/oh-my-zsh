dc40ccc6685d73cfb739647e809c1160fbf23aa1
Mohammad Mohtasham <mohm@yelp.com>
2019-06-26 17:43:01 -0700

    Renamed the theme in base.

--- base.sh
+++ base.sh
@@ -8,7 +8,7 @@ export ZSH="/nail/home/mohm/github/vim345/oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="vim345"
+ZSH_THEME="mhmd"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
